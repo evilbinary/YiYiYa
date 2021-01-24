4e7439f8986277cc833dffed149c9746b4e2e1dd
evilbinary <rootntsd@gmail.com>
2021-01-24 12:37:37 +0800

    add arm

---
+++ arch/arm/interrupt.c
@@ -0,0 +1,27 @@
+/*******************************************************************
+* Copyright 2021-2080 evilbinary
+* 作者: evilbinary on 01/01/20
+* 邮箱: rootdebug@163.com
+********************************************************************/
+
+#include "../interrupt.h"
+
+extern boot_info_t* boot_info;
+
+
+void interrupt_init() {
+   
+}
+
+void timer_init(int hz) {
+  
+}
+
+void interrutp_regist(u32 vec, interrupt_handler_t handler) {
+ 
+}
+
+void interrutp_set(int i) {
+  
+}
+
