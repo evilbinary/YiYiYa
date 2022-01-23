73ab47bbbf2c24f1283d7e26d396969f3eda56ad
evilbinary <rootntsd@gmail.com>
2022-01-23 22:53:15 +0800

    fix dirent

--- libs/include/c/dirent.h
+++ libs/include/c/dirent.h
@@ -13,6 +13,28 @@ extern "C" {
 #define __NEED_size_t
 #endif
 
+
+enum {
+  DT_UNKNOWN = 0,
+#define DT_UNKNOWN DT_UNKNOWN
+  DT_FIFO = 1,
+#define DT_FIFO DT_FIFO
+  DT_CHR = 2,
+#define DT_CHR DT_CHR
+  DT_DIR = 4,
+#define DT_DIR DT_DIR
+  DT_BLK = 6,
+#define DT_BLK DT_BLK
+  DT_REG = 8,
+#define DT_REG DT_REG
+  DT_LNK = 10,
+#define DT_LNK DT_LNK
+  DT_SOCK = 12,
+#define DT_SOCK DT_SOCK
+  DT_WHT = 14
+#define DT_WHT DT_WHT
+};
+
 struct dirent {
 	u64 d_ino;
 	u64 d_off;
@@ -24,7 +46,9 @@ struct dirent {
 
 typedef struct DIR {
 	int fd;
-	int index;
+	int buffer_pos;
+	int buffer_end;
+	char buffer[256];
 } DIR;
 
 
--- libs/libc/dirent.c
+++ libs/libc/dirent.c
@@ -10,29 +10,32 @@ DIR *opendir(const char *dirname) {
   }
   DIR *dir = (DIR *)malloc(sizeof(DIR));
   dir->fd = fd;
-  dir->index = -1;
+  dir->buffer_pos = 0;
   return dir;
 }
 
 int closedir(DIR *dir) {
   if (dir && (dir->fd != -1)) {
-    return close(dir->fd);
+    int ret = close(dir->fd);
+    free(dir);
+    return ret;
   } else {
     return -EBADF;
   }
 }
 
-struct dirent *readdir(DIR *dirp) {
-  static struct dirent ent;
-  int ret = ya_readdir(dirp->fd, ++dirp->index, &ent);
-  if (ret < 0) {
-    errno = -ret;
-    memset(&ent, 0, sizeof(struct dirent));
-    return NULL;
+struct dirent *readdir(DIR *dir) {
+  struct dirent *de;
+  if (dir->buffer_pos >= dir->buffer_end) {
+    int len = ya_readdir(dir->fd, dir->buffer, sizeof dir->buffer);
+    if (len <= 0) {
+      if (len < 0 && len != -ENOENT) errno = -len;
+      return 0;
+    }
+    dir->buffer_end = len;
+    dir->buffer_pos = 0;
   }
-  if (ret == 0) {
-    memset(&ent, 0, sizeof(struct dirent));
-    return NULL;
-  }
-  return &ent;
+  de = (void *)(dir->buffer + dir->buffer_pos);
+  dir->buffer_pos += de->d_reclen;
+  return de;
 }
