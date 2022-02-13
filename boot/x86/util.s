97d93d81ab3d58059f1f2c8b1a5465aede760147
qingshu-yu <99584740+qingshu-yu@users.noreply.github.com>
2022-02-13 17:00:06 +0800

    Update util.s
    添加注释

--- boot/x86/util.s
+++ boot/x86/util.s
@@ -1,26 +1,30 @@
-;;设置光标位置  DH=列，DL=行
+; 设置光标位置
+; int 10h ah=02h表设定光标位置 dh=游标的列数 dl=游标的行数 bh=页码
 set_cursor:
-    mov ah,0x02 ;光标位置初始化
+    mov ah,0x02
     mov bh,0
     int 0x10
     ret
+    
 reset:
     mov ah,0x00
     mov al,0x01
     int 0x10
     ret
 
-;;清除屏幕 
+; 清除屏幕
+; int 10h ah=06h表滚动窗口 al=滚动的列数，若为0则实现清空屏幕功能
 cls:
-    mov ah,0x06   ;清除屏幕					
+    mov ah,0x06			
     mov al,0
-    mov cx,0   
+    mov cx,0
     mov dx,0xffff  
-    mov bh,0x0f ;属性为白字
+    mov bh,0x0f
     int 0x10
     ret
 
-;;打印一个字符 al=ascii值
+; 打印一个字符
+; int 10h ah=0eh al=打印字符 bh＝页码 bl=前景色
 print_char:
     mov ah,0eh
     mov bx,0007h
